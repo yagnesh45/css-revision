78fb780f56df53b52c3e38c4f5521d8bdd133881
Yagnesh Patil <yagnesh.patil@CDSYS.LOCAL>
2021-05-06 20:18:50 +0530

    ADDED: first.js for javascript

---
+++ first.js
@@ -0,0 +1,9 @@
+var number = 2
+
+function square(num) {
+    ans = num * num;
+    return answer;
+}
+
+var answer = square(number);
+document.write("<p style=\"color:green;\"> Yay, we can print here out answer: " + answer + "</p>")
