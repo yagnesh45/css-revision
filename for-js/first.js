b463524cb311eb98b009e02891b9702eadee5546
Yagnesh Patil <yagnesh.patil@CDSYS.LOCAL>
2021-05-08 12:12:52 +0530

    UPDATED: Directory Structure for all the html, js and css files

--- for-js/first.js
+++
@@ -1,9 +0,0 @@
-var number = 2
-
-function square(num) {
-    ans = num * num;
-    return answer;
-}
-
-var answer = square(number);
-document.write("<p style=\"color:green;\"> Yay, we can print here out answer: " + answer + "</p>")
---
+++ js/first.js
@@ -0,0 +1,9 @@
+var number = 15
+
+function square(num) {
+    ans = num * num;
+    return ans;
+}
+
+var answer = square(number);
+console.log("%c" + answer, "font-weight:bold; font-size:2rem; color:green;")
