b2f04e5caac53eb8842b7d59256f1cbac2d686f0
Yagnesh Patil <yagnesh.patil@CDSYS.LOCAL>
2021-05-27 00:31:43 +0530

    ADDED: Code for closures in first.js

--- js/first.js
+++ js/first.js
@@ -42,3 +42,54 @@ console.log(b);
 // anything above initialization of such variables is temporal dead zone for those variables.
 let b = 100;
 ************************************************/
+
+// Program 4: example of closures
+// Closure is function which bundles along with the lexical environment.
+// It can access outer function's scope from inner closure function.
+/* ********************************************
+// Example 1:
+function x() {
+  var a = 7;
+
+  function y() {  // Closure function
+    console.log("Variable from x():" + a);
+  }
+  y();
+}
+x();
+
+// Example 2: Returning a closure function in a variable.
+function x() {
+  var a = 8;
+
+  function y() {
+    console.log("Function Y:" + a);
+  }
+
+  console.log("Function X");
+  return y;  // will return the whole fun y()
+}
+
+var func = x();
+console.log(func);
+func();
+
+//  Example 3: One level deeper
+function x() {
+  var a = 100;
+
+  function y() {
+    var b = 400;
+
+    function z() {
+      console.log("Function Z:", a, b);
+    }
+    z();
+  }
+  y();
+}
+
+x();
+
+***********************************************/
+
